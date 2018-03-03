c7cf6ee549ccceb795a36f7ca65e838213cbd935
Anna Sobolewska <anna@whistle.com>
2018-03-02 18:11:05 -0800

    Add basic slack commands

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ require 'sinatra'
 get '/ping' do
   'pong'
 end
+
+post '/getaway' do
+  message = params[:text].gsub(params[:trigger_word], '').strip
+  action, _option = message.split(' ').map { |c| c.strip.downcase }
+
+  case action
+  when 'help'
+    "Available commands: _'help'_, _'delays'_, _'elevator'_, _'departsouth'_, _'departnorth'_"
+  when 'delays'
+    'delays'
+  when 'elevator'
+    'elevator'
+  when 'departnorth'
+    'departnorth'
+  when 'departsouth'
+    'departsouth'
+  end
+end
